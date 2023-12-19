9fa7db3b3d40b4d683dc642d2b1cc9b5a94f7728
Waliyu Habeeb <Ademolawaliyu1@gmail.com>
2023-12-19 02:47:50 -0800

    adding header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import SignIn from './Pages/SignIn'
 import SignUp from './Pages/SignUp'
 import Profile from './Pages/Profile'
+import Header from './components/Header'
 
-export default function App(){
-  return <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<SignIn />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/profile' element= {<Profile />} />
-      </Routes>
+
+
+const App = () => {
+  return (<BrowserRouter>
+
+
+    <Header />
+    <Routes>
+
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<SignIn />} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
   </BrowserRouter>
+
+
+  )
 }
+
+export default App
+
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const Home = () => {
   return (
     <div>
+        
+        <h1>hi</h1>
       
     </div>
   )
