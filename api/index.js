51043f0772ed6e254c4752bc5cbc8f2903665eeb
Waliyu Habeeb <Ademolawaliyu1@gmail.com>
2023-12-19 04:07:00 -0800

    middleware and function

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
+import user from './routes/user.route.js'
+import auth from './routes/auth.route.js'
+import { resolveSoa } from 'dns'
 
 mongoose
     .connect(process.env.MONGO)
@@ -14,6 +17,20 @@ mongoose
 
 
 const app = express()
+app.use(express.json())
+app.use('/app', user )
+app.use('/app/auth', auth)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error'
+
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    })
+})
 
 app.listen(3000, () => {
     console.log('server is listenin')
